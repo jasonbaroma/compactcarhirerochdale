86f55d1e4f232a740450245edc01996ad08630e2
jasonbaroma <jasonbaroma@gmail.com>
2026-06-16 17:15:10 +0800

    Generate site content for compactcarhirerochdale

---
+++ lib/location-links.ts
@@ -0,0 +1,45 @@
+﻿import { slugifyLocation } from "@/lib/utils"
+
+export const mainLocationLink = {
+  label: "Rochdale",
+  href: "/",
+}
+
+const LOCATION_NAMES = [
+  "Oldham",
+  "Bury",
+  "Heywood",
+  "Middleton",
+  "Littleborough",
+  "Todmorden",
+  "Ashton-under-Lyne",
+  "Manchester",
+] as const
+
+export const nearbyLocationLinks = LOCATION_NAMES.map((label, index) => ({
+  index: index + 1,
+  label,
+  href: `/${slugifyLocation(label)}`,
+}))
+
+export function buildLocationLinks(currentLocationLabel?: string) {
+  return [mainLocationLink, ...getNearbyLocationLinks(currentLocationLabel)]
+}
+
+export function getNearbyLocationLinks(currentLocationLabel?: string) {
+  const currentSlug = currentLocationLabel
+    ? slugifyLocation(currentLocationLabel)
+    : ""
+
+  return nearbyLocationLinks.filter(
+    (location) => location.href !== `/${currentSlug}`,
+  )
+}
+
+export const locationSlugEntries = LOCATION_NAMES.map((label, index) => ({
+  index: index + 1,
+  label,
+  slug: slugifyLocation(label),
+}))
+
+
